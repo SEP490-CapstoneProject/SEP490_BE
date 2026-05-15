8456c9062e6912676b2d869e8b320a34a9aecea1
trangtanduoc <trangtanduoc@gmail.com>
2026-05-15 18:22:54 +0700

    refactor(notification): streamline notification handling by removing message type checks and simplifying category resolution

--- src/Services/Notification/Notification.Domain/Constants/NotificationTypeGroups.cs
+++ src/Services/Notification/Notification.Domain/Constants/NotificationTypeGroups.cs
@@ -2,21 +2,15 @@ namespace Notification.Domain.Constants;
 
 public static class NotificationTypeGroups
 {
-    public static readonly string[] CommunityTypes = new[]
-    {
+    public static readonly string[] CommunityTypes =
+    [
         "COMMUNITY",
         "POST_FAVORITE",
         "COMMUNITY_REPORT_REVIEW"
-    };
-
-    public static readonly string[] MessageTypes = new[]
-    {
-        "CHAT_MESSAGE"
-    };
+    ];
 
     public const string CommunityCategory = "community";
     public const string SystemCategory = "system";
-    public const string MessageCategory = "message";
 
     public static bool IsCommunityType(string? type)
     {
@@ -28,16 +22,6 @@ public static class NotificationTypeGroups
         return CommunityTypes.Contains(type, StringComparer.OrdinalIgnoreCase);
     }
 
-    public static bool IsMessageType(string? type)
-    {
-        if (string.IsNullOrWhiteSpace(type))
-        {
-            return false;
-        }
-
-        return MessageTypes.Contains(type, StringComparer.OrdinalIgnoreCase);
-    }
-
     public static string ResolveCategory(string? type)
-        => IsCommunityType(type) ? CommunityCategory : IsMessageType(type) ? MessageCategory : SystemCategory;
+        => IsCommunityType(type) ? CommunityCategory : SystemCategory;
 }
